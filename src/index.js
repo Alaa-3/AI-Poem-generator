09b02c0e6574c560d392966e8d041bb19f69ff4d
Alaa <helwani@outlook.de>
2024-03-06 17:14:29 +0100

    Added the AI API integration

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ function generatePoem(event) {
   event.preventDefault();
 
   let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "fo2446b2eft0245a64a3b4a8ab6c7ec1";
   let context =
     "You are a romantic Poem expert and love to write short poems. Your mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the  poem with `SheCodes AI` inside <strong> element at the end of the poem and NOT at the beginning.";
   let prompt = `User instructions: Generate a poem  about ${instructionsInput.value}`;
